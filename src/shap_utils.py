80c1b7f68f5d43e7377618ad706c4db43748411b
Muhammad Rizqi <74269013+havelock01@users.noreply.github.com>
2025-07-20 22:31:18 +0700

    push to git, sebelum perubahan baru

--- src/shap_utils.py
+++ src/shap_utils.py
@@ -1,14 +1,26 @@
 import shap
 import matplotlib.pyplot as plt
+import numpy as np
 
 def generate_shap_plot(model, df_encoded, selected_index, class_index, feature_names):
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(df_encoded)
 
     fig, ax = plt.subplots()
+
+    if isinstance(shap_values, list):
+        values = shap_values[class_index][selected_index]
+    else:
+        values = shap_values[selected_index]
+
+    if isinstance(explainer.expected_value, (list, np.ndarray)):
+        base_value = explainer.expected_value[class_index]
+    else:
+        base_value = explainer.expected_value
+
     shap.plots.waterfall(shap.Explanation(
-        values=shap_values[class_index][selected_index],
-        base_values=explainer.expected_value[class_index],
+        values=values,
+        base_values=base_value,
         data=df_encoded.iloc[selected_index],
         feature_names=feature_names
     ), show=False)
